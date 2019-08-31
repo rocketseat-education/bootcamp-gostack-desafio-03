54b745ae1018cd121431fdda7ca17de7af44f1db
Guilherme Gomes <Tequelequeteque@users.noreply.github.com>
2019-08-31 16:04:20 -0300

    Update User.js

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -26,7 +26,7 @@ class User extends Model {
 
   static associate(models) {
     this.hasMany(models.Meetup, { foreignKey: 'user_id' });
-    this.hasMany(models.Subscription);
+    this.hasMany(models.Subscription,{ foreignKey: 'user_id' });
   }
 
   checkPassword(password) {
